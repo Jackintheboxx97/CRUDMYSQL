eded3a145f94d4a6483f1ab632e6ab8fd28ba5dc
Jack in the Boxx <kdijahsweets@gmail.com>
2020-04-27 18:58:50 -0500

    Add project files.

---
+++ CRUDMYSQL/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace CRUDMYSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    // Server=localhost;Database=bestbuy;Uid=root;Pwd=password;
+}
